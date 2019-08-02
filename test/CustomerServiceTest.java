8de95db20bb5552e0ee28bdcb089ba5ce21a8aaa
mark <mark@nology.io>
2019-08-02 11:44:30 +0100

    add star markers to check CustomerDB is not called

--- test/CustomerServiceTest.java
+++ test/CustomerServiceTest.java
@@ -1,22 +1,33 @@
-import org.junit.jupiter.api.*;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomerServiceTest {
 
-    public CustomerService service;
+    @Mock
+    CustomerDB customerDBMock;
+
+    @InjectMocks
+    CustomerService service;
 
     @BeforeEach
     public void setup(){
         System.out.println("in CustomerServiceTest.setup");
-        service = new CustomerService(new CustomerDB());
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void testAddCustomer() {
         System.out.println("in CustomerServiceTest.testAddCustomer");
+        when(customerDBMock.save(any(Customer.class))).thenReturn(true);
         Customer customer = new Customer();
         assertTrue( service.addCustomer(customer) );
     }
 }
+
+
 
