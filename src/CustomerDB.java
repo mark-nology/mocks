09cfa411f557c184dead444f2f798e07f6b89ab4
mark <mark@nology.io>
2019-08-02 11:46:03 +0100

    add star markers to check CustomerDB is not called

--- src/CustomerDB.java
+++ src/CustomerDB.java
@@ -1,12 +1,12 @@
 public class CustomerDB {
     public boolean customerExists(Customer customer){
-        System.out.println("    in CustomerDB.customerExists");
+        System.out.println("    *** in CustomerDB.customerExists");
         // some logic
         return false;
     }
 
     public boolean save(Customer customer) {
-        System.out.println("    in CustomerDB.save");
+        System.out.println("    *** in CustomerDB.save");
         // use http etc to save the customer in the remote database
         // if save unsuccessful return false, else
         return true;
